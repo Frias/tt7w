4048b158d1e296d40136d56165c5d2a87974dc69
Vitor Frias <dofriascomurgencia@gmail.com>
2012-08-16 23:37:56 +0100

    Inicial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>tt7w</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+<div class="container">
+  <div class="header"><a href="#"><img src="img/logo.jpg" alt="logo" name="logo" width="180" height="90" id="logo" style="background-color: #8090AB; display:block;" /></a> 
+    <!-- end .header --></div>
+  <div class="content">
+    <h1>Bem-vindo</h1>
+    <p>Esta página vai ser o layout base do &quot;projecto&quot;.</p>
+    <!-- end .content --></div>
+  <div class="footer">
+    <p>Apenas para aprender mais de PHP, SQL e CSS bem como as nornas XHTML 1.0</p>
+    <!-- end .footer --></div>
+<!-- end .container --></div>
+</body>
+</html>
