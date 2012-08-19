38535940891b52e1896cc27e25d41965222c453a
Vitor Frias <dofriascomurgencia@gmail.com>
2012-08-19 15:03:53 +0100

    Edited the head file to be uniform.

--- head.php
+++ head.php
@@ -1,8 +1,8 @@
 <table>
-	<tr>
+	<tr width="100%">
 		<td width="180" align="center"><a href="#"><img src="img/logo.jpg" alt="logo" name="logo" width="180" height="90" id="logo" /></a>
 		</td>
-		<td width="638" align="center"><h1>tt7w</h1>
+		<td width="100%" align="center"><h1>tt7w</h1>
 		</td>
 	</tr>
 </table>
