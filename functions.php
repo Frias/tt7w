fc5dceef8f53a65ff4cb088fc32b636b7b0de1b6
Vitor Frias <dofriascomurgencia@gmail.com>
2012-08-21 17:47:36 +0100

    Corrected functions.php.

--- functions.php
+++ functions.php
@@ -1,4 +1,4 @@
-<?
+<?php
 function valid_email($str){
 	return ( ! preg_match("/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/ix", $str)) ? FALSE : TRUE;
 }
@@ -30,7 +30,7 @@ function random_string($type = 'alnum', $len = 8){
 }
 
 function checkUnique($table, $field, $compared){
-	$sql = "SELECT  '.$field.' FROM '.$table.' WHERE '.$field.'" = "'.$compared.";
+	$sql = "SELECT  '".$field."' FROM '".$table."' WHERE '".$field."' = '".$compared.'"";
 	$res = mysqli_query($cn,$sql);
 	if(mysqli_num_rows($res)==0){
 		return TRUE;
