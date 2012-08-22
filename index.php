80835d7a0e9aa86ceff7a3ef137a44df21762a71
Vitor Frias <dofriascomurgencia@gmail.com>
2012-08-22 18:38:48 +0100

    Edited index.php to be a restricted page.

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php
+	include('includes/db.php');
+	include('includes/functions.php');
+	session_start();
+	checkLogin('1 2');
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
