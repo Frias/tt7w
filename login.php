e01616f3f52323f4523499e54a33e4f8469cb150
Vitor Frias <dofriascomurgencia@gmail.com>
2012-08-22 14:41:09 +0100

    Improved login.php.

--- login.php
+++ login.php
@@ -1,3 +1,32 @@
+<?php
+require_once('db.php');
+include('functions.php');
+
+	if(isset($_POST['Login'])){
+		if($_POST['username']!='' && $_POST['password']!=''){
+			$sql = "SELECT id, username, active FROM users WHERE username = '".$_POST['username']."' AND password = '".SHA1($_POST['password'])."'";
+			$query = mysqli_query($cn, $sql);
+			
+			if(mysqli_num_rows($query) == 1){
+				$row = mysqli_fetch_assoc($query);
+				if($row['active'] == 1){
+					$_SESSION['user'] = $row['id'];
+					$_SESSION['logged'] = TRUE;
+					header("Location: index.php");
+				}
+				else {
+					$error = 'Membro não ativo';
+				}
+			}
+			else {		
+				$error = 'Login falhou !';		
+			}
+		}
+		else {
+			$error = 'Usa ambos, username e password, para aceder';
+		}
+	}
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -16,8 +45,9 @@
 	<?php include("menu.php"); ?>
   </div>
   <div class="content">
+	<?php if(isset($error)){ echo $error;}?>
     <table>
-    	<form action="<? echo $_SERVER['PHP_SELF']; ?>" method="post">
+    	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 			<tr><td>Username:</td><td><input type="text" id="username" name="username" size="32" value="" /></td></tr>
 			<tr><td>Password:</td><td><input type="password" id="password" name="password" size="32" value="" /></td></tr>
 			<tr><td><input type="reset" name="Repor" value="Repor" /></td><td><input type="submit" name="Login" value="Login" /></td></tr>
