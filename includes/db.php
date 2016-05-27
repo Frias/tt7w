587426a0578f65813624ca4f72b4d73affbab930
Vitor Frias <dofriascomurgencia@gmail.com>
2016-05-27 22:48:57 +0100

    Edited db credentials

--- includes/db.php
+++ includes/db.php
@@ -1,4 +1,4 @@
 ﻿<?php
-	$cn=mysqli_connect("localhost", "tl", "divina","tl");
+	$cn=mysqli_connect("localhost", "tl", "downlink","tl");
 		if (!$cn) print ("Problemas na ligacao ao servidor Mysql");
 ?>
