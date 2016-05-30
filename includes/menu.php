1e1df2c8c83058bcdf2dd91c200053dec2772ed0
Vitor Frias <dofriascomurgencia@gmail.com>
2016-05-31 00:17:03 +0100

    Edited menu for logged in users

--- includes/menu.php
+++ includes/menu.php
@@ -1,3 +1,21 @@
+<?php
+$sql = "SELECT * FROM users WHERE id = '".mysqli_real_escape_string($cn,$_SESSION['user'])."'";
+$query = mysqli_query($cn, $sql);
+$row = mysqli_fetch_assoc($query);
+?>
+<table width="100%">
+	<tr>
+		<td align="left">
+			Ola, <a href="userdetails.php?id=<?php echo $row['id']?>"><?php echo $row['username']?></a>
+		</td>
+		<td align="right">
+			Mensagens
+		</td>
+		<td align="right">
+			<a href="logout.php">Sair</a>
+		</td>
+	</tr>
+</table>
 <table width="100%">
 	<tr align="center">
 		<td>
@@ -8,9 +26,6 @@
 		</td>
 		<td>
 			link3
-		</td>
-		<td>
-			<a href="logout.php">Sair</a>
 		</td>
 	</tr>
 </table>
